e5752f0fe238d1a7aa42ec15987a1052f32aa442
debdeeps <debdeep.bhu@gmail.com>
2020-05-19 16:48:10 +0200

    Update ReUsableMethods.java

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -7,7 +7,7 @@ public class ReUsableMethods {
 	
 	public static JsonPath rawToJson(String response)
 	{
-		JsonPath js =new JsonPath(response);
+		JsonPath js1 =new JsonPath(response);
 		return js1;
 	}
 }
